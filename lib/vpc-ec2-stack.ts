5611419c4044183641b5111aed445aa30855e977
qiaoli116 <qiaoli.116@gmail.com>
2023-06-09 16:34:09 +0000

    updates

--- lib/vpc-ec2-stack.ts
+++ lib/vpc-ec2-stack.ts
@@ -6,7 +6,6 @@ import { MyVpcConstructProps, MyVpcConstruct } from './MyVpcConstruct';
 
 export class VpcStack extends NestedStack {
     public readonly Vpc: ec2.Vpc;
-    public readonly Subnets: ec2.Subnet[] = [];
     constructor(scope: Construct, id: string, props?: {}) {
         super(scope, id, props);
         console.log(`VpcStack - AZs: ${this.availabilityZones}`)
@@ -14,15 +13,34 @@ export class VpcStack extends NestedStack {
         // Create a VPC
         this.Vpc = new ec2.Vpc(this, 'Vpc', {
             ipAddresses: ec2.IpAddresses.cidr(cidr),
-            maxAzs: 10,
-            // subnetConfiguration: [{
-            //     cidrMask: 24,
-            //     name: 'ingress',
-            //     subnetType: ec2.SubnetType.PUBLIC,
-            // }]
+            maxAzs: 3,
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: 'public-',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                },
+                {
+                    cidrMask: 24,
+                    name: 'private-isolated-',
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                }
+            ]
         });
         console.log(`number of public subnet ${this.Vpc.publicSubnets.length}`);
+        this.Vpc.publicSubnets.forEach((subnet, index) => {
+            console.log(`subnet ${index} = ${subnet.subnetId} / ${subnet.availabilityZone} / ${subnet.ipv4CidrBlock}`);
+        });
+        console.log(`number of isolated subnet ${this.Vpc.isolatedSubnets.length}`);
+        this.Vpc.isolatedSubnets.forEach((subnet, index) => {
+            console.log(`subnet ${index} = ${subnet.subnetId} / ${subnet.availabilityZone} / ${subnet.ipv4CidrBlock}`);
+        });
         console.log(`number of private subnet ${this.Vpc.privateSubnets.length}`);
+        this.Vpc.privateSubnets.forEach((subnet, index) => {
+            console.log(`subnet ${index} = ${subnet.subnetId} / ${subnet.availabilityZone} / ${subnet.ipv4CidrBlock}`);
+        });
+
+
         const azs = this.Vpc.availabilityZones
         azs.forEach((az, index) => {
             console.log(`AZ ${index} = ${az}`);
@@ -35,33 +53,41 @@ export class Ec2Stack extends NestedStack {
     constructor(scope: Construct, id: string, props: Ec2ConstructProps) {
         super(scope, id, props);
 
-        const img = new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023 });
-        console.log(`img: ${img}`);
+        const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+            vpc: props.vpc,
+            allowAllOutbound: true,
+        });
+        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access from the world');
+        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http access from the world');
+        const img = ec2.MachineImage.latestAmazonLinux2023();
+        console.log(`img: ${img.getImage(this).imageId}`);
         // Create an EC2 instance
         const ec2Instance = new ec2.Instance(this, 'Ec2', {
             instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
             vpc: props.vpc,
             machineImage: img,
+            securityGroup: securityGroup,
         });
+        // ec2Instance.addSecurityGroup(securityGroup);
     }
 }
 
 export class MainStack extends Stack {
     constructor(scope: Construct, id: string, props?: {}) {
         super(scope, id, props);
-        console.log(`MainStack - AZs: ${this.availabilityZones}`)
 
         // Create a nested VpcStack
-        const vpcStack = new VpcStack(this, 'VpcStack', props);
+        const vpcStack = new VpcStack(this, 'VpcStack');
 
         // Create an Ec2Stack and pass the subnet ID from the VpcStack
-        // const ec2Stack = new Ec2Stack(this, 'Ec2Stack', {
-        //     vpc: vpcStack.Vpc,
-        // });
+        const ec2Stack = new Ec2Stack(this, 'Ec2Stack', {
+            vpc: vpcStack.Vpc,
+        });
     }
 }
 
 
 export interface Ec2ConstructProps extends StackProps {
     vpc: ec2.IVpc;
+    subnets?: ec2.ISubnet[];
 }
